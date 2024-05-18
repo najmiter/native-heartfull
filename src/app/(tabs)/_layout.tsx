d95fbfb4b2a1535346e269c1f8e08a80c67bf1c6
najmiter <realnajmiter@gmail.com>
2024-05-18 19:43:58 +0500

    now we're somewhere

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -7,36 +7,44 @@ export default function TabLayout() {
     return (
         <Tabs
             screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: "#222",
+                    borderColor: "#222",
+                },
                 headerShown: true,
+                headerStyle: {
+                    backgroundColor: "#222",
+                },
+                headerTintColor: "#eee",
+                headerRight: () => (
+                    <Link href="/modal" asChild>
+                        <Pressable>
+                            {({ pressed }) => (
+                                <FontAwesome
+                                    name="circle"
+                                    size={25}
+                                    style={{
+                                        marginRight: 15,
+                                        opacity: pressed ? 0.5 : 1,
+                                        color: "#eee",
+                                    }}
+                                />
+                            )}
+                        </Pressable>
+                    </Link>
+                ),
             }}
         >
             <Tabs.Screen
                 name="index"
                 options={{
-                    title: "Tab w",
-                    headerRight: () => (
-                        <Link href="/modal" asChild>
-                            <Pressable>
-                                {({ pressed }) => (
-                                    <FontAwesome
-                                        name="info-circle"
-                                        size={25}
-                                        color={"black"}
-                                        style={{
-                                            marginRight: 15,
-                                            opacity: pressed ? 0.5 : 1,
-                                        }}
-                                    />
-                                )}
-                            </Pressable>
-                        </Link>
-                    ),
+                    title: "Heartfull",
                 }}
             />
             <Tabs.Screen
                 name="two"
                 options={{
-                    title: "Tab Two",
+                    title: "History",
                 }}
             />
         </Tabs>
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,26 @@
-import { StyleSheet, View, Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Text, Pressable, Button } from "react-native";
 
 export default function TabOneScreen() {
+    const [count, setCount] = useState<number>(0);
+    const target = 10;
+
+    const aspectRatio = count !== 0 ? 1 / (count / target) : 0;
+
+    function handleCounter() {
+        if (count % target === 0) setCount(1);
+        else setCount(count + 1);
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Tab yabli</Text>
+            <Button title="Reset" onPress={() => setCount(0)}></Button>
+            <Pressable onPress={handleCounter} style={[styles.container]}>
+                <View style={styles.counterCircle}>
+                    <Text style={styles.counterText}>{count}</Text>
+                    <View style={[styles.filler, { aspectRatio }]} />
+                </View>
+            </Pressable>
         </View>
     );
 }
@@ -11,17 +28,34 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: "100%",
         alignItems: "center",
         justifyContent: "center",
         backgroundColor: "#111",
     },
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
+    counterCircle: {
+        width: 200,
+        aspectRatio: 1,
+        backgroundColor: "#888",
+        borderRadius: 100,
+        position: "relative",
+        alignItems: "center",
+        justifyContent: "center",
+        overflow: "hidden",
     },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
+    counterText: {
+        color: "#222",
+        fontSize: 70,
+        fontFamily: "monospace",
+        fontWeight: "bold",
+        zIndex: 10,
+    },
+    filler: {
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        width: 200,
+        aspectRatio: 1,
+        backgroundColor: "#666",
     },
 });
